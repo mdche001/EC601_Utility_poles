872f2ba2352037699f05ad771df97382f3cf72c1
mdche001 <mdche@bu.edu>
2018-12-20 02:47:43 +0800

    model

---
+++ Detection/comp_detection/input_data.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+import numpy as np
+import os
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+def get_files(file_dir):
+    "return list of images and labels"
+
+    #initialize the image_list and label_list
+    image_list = []
+    label_list = []
+    count_poles = 0
+    count_others = 0
+
+    #Building the image_list and lable_list
+    for file in os.listdir(file_dir):
+        name = file.split(sep='.')
+        image_list.append(file_dir + file)
+        if name[0] == 'pole':
+            label_list.append(0)
+            count_poles += 1
+        else:
+            label_list.append(1)
+            count_others += 1
+    # print('There are %d poles\n ' %(count_poles))
+    # print('There are %d others ' %(count_others))
+
+    image_list = np.asarray(image_list)
+    label_list = np.asarray(label_list)
+
+    #shuffle the list
+    temp = np.array([image_list, label_list])
+    temp = temp.transpose()
+    np.random.shuffle(temp)
+
+    image_list = list(temp[:,0])
+    label_list = list(temp[:,1])
+    print(label_list)
+    label_list = [int(float(i)) for i in label_list]
+
+    return image_list, label_list
+
+def get_batch(image, label, image_W, image_H, batch_size, capacity):
+    "return image_batch with [batch_size, width, height, 3], dtype = tf.float32  label.batch with[batch_size], dtype = tf.int32"
+    '''
+    :parameter:
+        image,label: list from get
+        image_W：image width
+        image_H: image height
+        batch_size: batch size
+        capacity: queue size
+    '''
+
+    #convert to d-type
+    image = tf.cast(image, tf.string)
+    label= tf.cast(label, tf.int32)
+    train_list = [image,label]
+    #make input queue
+    input_queue = tf.train.slice_input_producer(train_list, shuffle=False)
+
+    #images tags
+    label = input_queue[1]
+
+    #load images
+    image = tf.read_file(input_queue[0])
+    image = tf.image.decode_jpeg(image, channels = 3)
+    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
+    image = tf.image.per_image_standardization(image)
+    image_batch, label_batch = tf.train.batch([image, label],
+                                                batch_size= batch_size,
+                                                num_threads= 64,
+                                                capacity= capacity)
+
+    #get batch
+    label_batch = tf.reshape(label_batch, [batch_size])
+    return  image_batch, label_batch
+
+
+# test input
+# import matplotlib.pyplot as plt
+#
+# BATCH_SIZE = 2
+# CAPACITY = 256
+# IMG_W = 208
+# IMG_H = 208
+#
+# # test input directory
+# # train_dir = 'D:/program/venv/data/test/'
+# #train_dir = 'D:/program/venv/data/train/'
+#
+# train_dir = os.getcwd()+'/venv/data/train/'
+# image_list, label_list = get_files(train_dir)
+# image_batch, label_batch = get_batch(image_list, label_list, IMG_W, IMG_H, BATCH_SIZE, CAPACITY)
+#
+# with tf.Session() as sess:
+#     i = 0
+#     coord = tf.train.Coordinator()
+#     threads = tf.train.start_queue_runners(coord= coord)
+#
+#     #security input from google guide
+#     try:
+#         while not coord.should_stop() and i < 1:
+#             img, label = sess.run([image_batch, label_batch])
+#
+#             for j in np.arange(BATCH_SIZE):
+#                 print('label: %d' %label[j])
+#                 plt.imshow(img[j,:,:,:])
+#                 plt.show()
+#             i += 1
+#     except tf.errors.OutOfRangeError:
+#         print('done!')
+#     finally:
+#         coord.request_stop()
+#     coord.join(threads)
---
+++ Detection/comp_detection/training.py
@@ -0,0 +1,119 @@
+import os
+import numpy as np
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+# import matplotlib.pyplot as plt
+
+import input_data
+import model
+
+
+N_CLASSES = 2
+IMG_W = 200
+IMG_H = 200
+BATCH_SIZE = 16
+CAPACITY = 200
+MAX_STEP = 3000
+learning_rate = 1e-4
+
+#data & logs directory
+train_dir = 'D:/program/venv/data/train/'
+test_dir = 'D:/program/venv/data/test/'
+logs_train_dir = 'D:/program/venv/logs/train/'
+
+def run_training():
+    train, train_label = input_data.get_files(train_dir)
+    train_batch, train_label_batch = input_data.get_batch(train,
+                                                          train_label,
+                                                          IMG_W,
+                                                          IMG_H,
+                                                          BATCH_SIZE,
+                                                          CAPACITY)
+    train_logits = model.inference(train_batch, BATCH_SIZE, N_CLASSES)
+    train_loss = model.losses(train_logits, train_label_batch)
+    train_op = model.training(train_loss, learning_rate)
+    train_acc = model.evaluation(train_logits, train_label_batch)
+
+    summary_op = tf.summary.merge_all()
+    sess = tf.Session()
+    train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+    saver = tf.train.Saver()
+
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    try:
+        for step in np.arange(MAX_STEP):
+            if coord.should_stop():
+                break
+            _, tra_loss, tra_acc = sess.run([train_op, train_loss, train_acc])
+
+            if step % 50 == 0:
+                print('Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc))
+                summary_str = sess.run(summary_op)
+                train_writer.add_summary(summary_str, step)
+
+            if step % 200 == 0 or (step + 1) == MAX_STEP:
+                chechpoint_path = os.path.join(logs_train_dir,'model.ckpt')
+                saver.save(sess, chechpoint_path, global_step=step)
+
+    except tf.errors.OutOfRangeError:
+        print('Done training -- epoch limit reached')
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
+    sess.close()
+
+def evaluate_all_image():
+    '''
+    Test all image against the saved models and parameters.
+    Return global accuracy of test_image_set
+    ##############################################
+    ##Notice that test image must has label to compare the prediction and real
+    ##############################################
+    '''
+    # you need to change the directories to yours.
+    N_CLASSES = 2
+    print('-------------------------')
+    test, test_label = input_data.get_files(test_dir)
+    BATCH_SIZE = len(test)
+    print('There are %d test images totally..' % BATCH_SIZE)
+    print('-------------------------')
+    test_batch, test_label_batch = input_data.get_batch(test,
+                                                        test_label,
+                                                        IMG_W,
+                                                        IMG_H,
+                                                        BATCH_SIZE,
+                                                        CAPACITY)
+    logits = model.inference(test_batch, BATCH_SIZE, N_CLASSES)
+    testloss = model.losses(logits, test_label_batch)
+    testacc = model.evaluation(logits, test_label_batch)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        print("Reading checkpoints...")
+        ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print('Loading success, global_step is %s' % global_step)
+        else:
+            print('No checkpoint file found')
+        print('-------------------------')
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        test_loss, test_acc = sess.run([testloss, testacc])
+        print('The model\'s loss is %.2f' % test_loss)
+        correct = int(BATCH_SIZE * test_acc)
+        print('Correct : %d' % correct)
+        print('Wrong : %d' % (BATCH_SIZE - correct))
+        print('The accuracy in test images are %.2f%%' % (test_acc * 100.0))
+    coord.request_stop()
+    coord.join(threads)
+    sess.close()
+
+if __name__ == '__main__':
+    evaluate_all_image()
